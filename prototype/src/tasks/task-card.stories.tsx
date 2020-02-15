316ffba9dac6b5abfa519326ca778fd4528d6982
Dieter Luypaert <dieterluypaert@gmail.com>
2020-02-15 22:18:35 +0100

    feat: give card a base style

--- prototype/src/tasks/task-card.stories.tsx
+++ prototype/src/tasks/task-card.stories.tsx
@@ -11,7 +11,9 @@ const metadata = {
 export default metadata;
 
 export const withText = (): React.ReactElement => (
-  <TaskCard
-    description={text('Description', 'The Unexpected Virtue of Ignorance')}
-  />
+  <div style={{ margin: '1em', display: 'flex', justifyContent: 'center' }}>
+    <TaskCard
+      description={text('Description', 'The Unexpected Virtue of Ignorance')}
+    />
+  </div>
 );
