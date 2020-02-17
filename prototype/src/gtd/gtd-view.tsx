10aea34953cba5749ab132fe4da3c97d2a64fd16
Dieter Luypaert <dieterluypaert@gmail.com>
2020-02-17 15:29:16 +0100

    feat: add drag constraints

--- prototype/src/gtd/gtd-view.tsx
+++ prototype/src/gtd/gtd-view.tsx
@@ -1,9 +1,13 @@
+import clamp from 'lodash/fp/clamp';
 import React, { ReactElement, useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
 
 import { Task } from '../tasks';
 import * as styles from './gtd-view.module.css';
+
+const X_BOUND_RIGHT = 20;
+const X_TO_ANGLE = -0.25;
 
 interface GtdViewProperties {
   task: Task;
@@ -16,13 +20,28 @@ export default function GtdView({ task }: GtdViewProperties): ReactElement {
   }
 
   const [{ x }, set] = useSpring(() => ({ x: 0 }));
-  const bind = useDrag(({ down, movement: [mx] }) => {
-    set({ x: down ? mx : 0 });
-  });
+  const bind = useDrag(
+    ({ down, movement: [mx] }) => {
+      set({ x: down ? mx : 0 });
+    },
+    {
+      bounds: { left: 0, right: X_BOUND_RIGHT },
+      rubberband: 0.125,
+    },
+  );
 
   return (
     <div className={styles.gtdViewContainer}>
-      <animated.div className={styles.cardWrapper} style={{ x }} {...bind()}>
+      <animated.div
+        className={styles.cardWrapper}
+        style={{
+          rotateY: x.interpolate(
+            (v) => `${clamp(0, X_BOUND_RIGHT, v) * X_TO_ANGLE}deg`,
+          ),
+          x,
+        }}
+        {...bind()}
+      >
         <div className={styles.card}>
           <div className={styles.cardDescription}>{task.description}</div>
           <div className={styles.cardActions}>
